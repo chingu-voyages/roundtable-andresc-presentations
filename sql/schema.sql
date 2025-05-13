20189bb1ee04f1fa9db26198f982583829fb5a53
Andres Court <andres@andrescourt.com>
2025-05-12 22:58:11 -0500

    feat(schema): tables script

---
+++ sql/schema.sql
@@ -0,0 +1,22 @@
+CREATE TABLE country (
+    id char(2) PRIMARY KEY,
+    name VARCHAR(255),
+
+    unique (name)
+);
+
+CREATE TABLE client (
+    id uuid PRIMARY KEY default gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    surname VARCHAR(255) NOT NULL,
+    country_id CHAR(2) NOT NULL REFERENCES country on delete restrict
+);
+
+CREATE TABLE account (
+    id SERIAL PRIMARY KEY,
+    number VARCHAR(255) NOT NULL,
+    client_id uuid NOT NULL REFERENCES client (id) on delete restrict,
+    ammount DECIMAL(15,6),
+
+    unique (number)
+);

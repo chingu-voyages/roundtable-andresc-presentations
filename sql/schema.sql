af42516b6683b7a1b0e39b246e5179bd933b1d43
Andres Court <andres@andrescourt.com>
2025-05-13 15:49:44 -0500

    feat(sql): reformat file

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,22 +1,22 @@
 CREATE TABLE country (
-    id char(2) PRIMARY KEY,
-    name VARCHAR(255),
+  id char(2) PRIMARY KEY,
+  name VARCHAR(255),
 
-    unique (name)
+  unique (name)
 );
 
 CREATE TABLE client (
-    id uuid PRIMARY KEY default gen_random_uuid(),
-    name VARCHAR(255) NOT NULL,
-    surname VARCHAR(255) NOT NULL,
-    country_id CHAR(2) NOT NULL REFERENCES country on delete restrict
+  id uuid PRIMARY KEY default gen_random_uuid(),
+  name VARCHAR(255) NOT NULL,
+  surname VARCHAR(255) NOT NULL,
+  country_id CHAR(2) NOT NULL REFERENCES country on delete restrict
 );
 
 CREATE TABLE account (
-    id SERIAL PRIMARY KEY,
-    number VARCHAR(255) NOT NULL,
-    client_id uuid NOT NULL REFERENCES client (id) on delete restrict,
-    ammount DECIMAL(15,6),
+  id SERIAL PRIMARY KEY,
+  number VARCHAR(255) NOT NULL,
+  client_id uuid NOT NULL REFERENCES client (id) on delete restrict,
+  ammount DECIMAL(15,6),
 
-    unique (number)
+  unique (number)
 );
